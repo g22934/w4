54d714cef1b524010c3848822308500382d93f98
haruka <144975485+g22934@users.noreply.github.com>
2023-09-28 11:18:34 +0900

    Update main.js

--- assessment-start/main.js
+++ assessment-start/main.js
@@ -11,4 +11,23 @@ const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. W
 //配列は[]で囲んで宣言
 const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 const insertY = ['the soup kitchen','Disneyland','the White House'];
+const insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
