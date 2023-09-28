c497d7f1f7b7a64f514adeff59ca94d2234a2a53
haruka <144975485+g22934@users.noreply.github.com>
2023-09-28 11:17:14 +0900

    Update main.js

--- assessment-start/main.js
+++ assessment-start/main.js
@@ -10,5 +10,5 @@ function randomValueFromArray(array){
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 //配列は[]で囲んで宣言
 const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-
+const insertY = ['the soup kitchen','Disneyland','the White House'];
 
