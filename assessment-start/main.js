c0e80bccd746c54ce02ecbbbf368f5ecb54439df
haruka <144975485+g22934@users.noreply.github.com>
2023-09-28 14:13:53 +0900

    Update main.js

--- assessment-start/main.js
+++ assessment-start/main.js
@@ -9,9 +9,9 @@ function randomValueFromArray(array){
 //文字は''で囲む.最後は;つける
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 //配列は[]で囲んで宣言
-const insertX = ['私', 'アンミカ', 'マツコ'];
-const insertY = ['台所', 'ディズニーランド', 'ホワイトハウス'];
-const insertZ = ['遊んだ', '眠った', '走った'];
+const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+const insertY = ['the soup kitchen','Disneyland','the White House'];
+const insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
